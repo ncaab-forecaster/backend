2a643d4cff4f33565e66e3b940eda7071577add5
William Connatser <43946230+WilliamConnatser@users.noreply.github.com>
2020-01-03 01:05:47 -0600

    hooked up data science API- changed team id to sportreference module abbreviations

--- app.py
+++ app.py
@@ -2,11 +2,16 @@
 # Needed because pylint is incompatible with SQL Alchemy for some reason...
 
 import os
-from datetime import datetime
+import requests
+import json
+from datetime import datetime as dt
+from datetime import timedelta
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS, cross_origin
 from sportsreference.ncaab.teams import Teams
+from sportsreference.ncaab.boxscore import Boxscores
+from sportsreference.ncaab.rankings import Rankings
 
 app = Flask(__name__)
 CORS(app)
@@ -18,6 +23,18 @@ db = SQLAlchemy(app)
 # pep8 moves this line to the top, but we need db to be declared first
 from models import Game, Team  # nopep8
 
+
+@app.cli.command("test")
+def test():
+    # teams = list(Teams(2019))
+    # teams.sort(key=lambda x: x.name)
+    # for team in teams:
+    #     print(f'{team.name} = {team.simple_rating_system}')
+    today = dt.today()
+    start = today.replace(hour=0, minute=0, second=0, microsecond=0)
+    end = start + timedelta(1)
+    print(start, end)
+
 # Create "flask seed" command which seeds the database with all teams and games.
 @app.cli.command("seed")
 def seed_database():
@@ -25,7 +42,7 @@ def seed_database():
     teams = list(Teams(2019))
     teams.sort(key=lambda x: x.name)
     for team in teams:
-        new_team = Team(name=team.name)
+        new_team = Team(id=team.abbreviation, name=team.name)
         db.session.add(new_team)
     try:
         db.session.commit()
@@ -42,70 +59,164 @@ def hello():
 def projections():
     home = request.args.get('home')
     away = request.args.get('away')
-    try:
-        if home is None and away is None:
-            # Todo: Return actual data from database
-            # Right now it is empty, so we're returning dummy data
 
-            # games = Game.query.filter_by(
-            #     date=datetime.today())
-            # return jsonify([g.serialize() for g in games])
+    if home is None and away is None:
+        today = dt.today()
+        today_start = today.replace(
+            hour=0, minute=0, second=0, microsecond=0)
+        today_end = today_start + timedelta(1)
 
-            print(datetime.today())
+        games = db.session.query(Game).filter(
+            Game.date > today_start, Game.date < today_end).all()
 
-            return jsonify([
-                {
-                    'id': 1,
-                    'date': datetime.today(),
-                    'home_id': 1,
-                    'away_id': 2,
-                    'home_name': 'Gonzaga',
-                    'away_name': 'Duke',
-                    'home_score': 89,
-                    'away_score': 67,
-                    'home_projection': 92,
-                    'away_projection': 85
-                },
-                {
-                    'id': 2,
-                    'date': datetime.today(),
-                    'home_id': 3,
-                    'away_id': 4,
-                    'home_name': 'Kansas',
-                    'away_name': 'Oregon',
-                    'home_score': 83,
-                    'away_score': 62,
-                    'home_projection': 91,
-                    'away_projection': 62
-                },
-                {
-                    'id': 2,
-                    'date': datetime.today(),
-                    'home_id': 5,
-                    'away_id': 6,
-                    'home_name': 'Ohio State',
-                    'away_name': 'Baylor',
-                    'home_score': 67,
-                    'away_score': 49,
-                    'home_projection': 59,
-                    'away_projection': 51
-                }
-            ])
+        # If today's games have not been retrieved yet then do so from sports
+        # And get the projected score from the data science API
+        if len(games) == 0:
+            games_today = Boxscores(dt.today())
+            games_today = games_today.games
 
-        elif home is None or away is None:
-            return ("'home' and 'away' are necessary query parameters when not using the 'all' query parameter")
-        else:
-            # Todo: Compare two random teams without inserting the game into the database
-            return jsonify({
-                'home_id': 5,
-                'away_id': 6,
-                'home_name': 'Ohio State',
-                'away_name': 'Baylor',
-                'home_projection': 69,
-                'away_projection': 55
-            })
-    except Exception as e:
-        return(str(e))
+            rankings = Rankings()
+            rankings = rankings.current
+
+            for date in games_today.keys():
+                for game in games_today[date]:
+                    home_team = db.session.query(Team).filter(
+                        Team.name == game["home_name"]).first()
+                    away_team = db.session.query(Team).filter(
+                        Team.name == game["away_name"]).first()
+
+                    if home_team is None:
+                        home_team = Team(
+                            id=game["home_abbr"], name=game["home_name"])
+                        db.session.add(home_team)
+                        try:
+                            db.session.commit()
+                            home_team = db.session.query(Team).filter(
+                                Team.name == game["home_name"]).first()
+                        except:
+                            db.session.rollback()
+
+                    elif away_team is None:
+                        away_team = Team(
+                            id=game["away_abbr"], name=game["away_name"])
+                        db.session.add(away_team)
+
+                        try:
+                            db.session.commit()
+                            away_team = db.session.query(Team).filter(
+                                Team.name == game["away_name"]).first()
+                        except:
+                            db.session.rollback()
+
+                    data = {
+                        "year": dt.today().year,
+                        "month": dt.today().month,
+                        "day": dt.today().day,
+                        "home_name": home_team.name,
+                        "home_rank": float(rankings.get(home_team.id.lower(), 0)),
+                        "away_name": away_team.name,
+                        "away_rank": float(rankings.get(away_team.id.lower(), 0))
+                    }
+
+                    res = requests.post(
+                        "http://ncaab.herokuapp.com/", data=json.dumps(data))
+                    res = res.json()
+
+                    home_projection = res["home_score"]
+                    away_projection = res["away_score"]
+
+                    new_game = Game(
+                        date=date, home=home_team.id, away=away_team.id, home_projection=home_projection, away_projection=away_projection)
+                    db.session.add(new_game)
+                    games.append(new_game)
+
+            # TODO: Test this out...
+            # Update yesterday's games with the scores
+            # yesterday_start = today_start - timedelta(1)
+            # games_yesterday = Boxscores(yesterday_start, today_start)
+            # games_yesterday = games_yesterday.games
+
+            # for date in games_yesterday.keys():
+            #     for game in games_yesterday[date]:
+            #         home_team = db.session.query(Team).filter(
+            #             Team.name == game["home_name"]).first()
+            #         away_team = db.session.query(Team).filter(
+            #             Team.name == game["away_name"]).first()
+            #         print(home_team, away_team)
+            #         applicable_game = db.session.query(Game).filter(
+            #             Game.date > yesterday_start, Game.date < today_start, Game.home == home_team.id, Game.away == away_team.id).first()
+            #         print(applicable_game)
+            #         applicable_game.home_score = game["home_score"]
+            #         applicable_game.away_score = game["away_score"]
+
+            try:
+                db.session.commit()
+            except:
+                db.session.rollback()
+        games_without_names = [g.serialize() for g in games]
+        games_with_names = []
+        for game in games_without_names:
+            home_team = db.session.query(Team).filter(
+                Team.id == game["home_id"]).first()
+            away_team = db.session.query(Team).filter(
+                Team.id == game["away_id"]).first()
+            game["home_name"] = home_team.name
+            game["away_name"] = away_team.name
+            games_with_names.append(game)
+        return jsonify(games_with_names)
+
+        # return jsonify([
+        #     {
+        #         'id': 1,
+        #         'date': dt.today(),
+        #         'home_id': 1,
+        #         'away_id': 2,
+        #         'home_name': 'Gonzaga',
+        #         'away_name': 'Duke',
+        #         'home_score': 89,
+        #         'away_score': 67,
+        #         'home_projection': 92,
+        #         'away_projection': 85
+        #     },
+        #     {
+        #         'id': 2,
+        #         'date': dt.today(),
+        #         'home_id': 3,
+        #         'away_id': 4,
+        #         'home_name': 'Kansas',
+        #         'away_name': 'Oregon',
+        #         'home_score': 83,
+        #         'away_score': 62,
+        #         'home_projection': 91,
+        #         'away_projection': 62
+        #     },
+        #     {
+        #         'id': 2,
+        #         'date': dt.today(),
+        #         'home_id': 5,
+        #         'away_id': 6,
+        #         'home_name': 'Ohio State',
+        #         'away_name': 'Baylor',
+        #         'home_score': 67,
+        #         'away_score': 49,
+        #         'home_projection': 59,
+        #         'away_projection': 51
+        #     }
+        # ])
+
+    elif home is None or away is None:
+        return ("'home' and 'away' are necessary query parameters when not using the 'all' query parameter")
+    else:
+        # Todo: Compare two random teams without inserting the game into the database
+        # Todo: Integrate DS API
+        return jsonify({
+            'home_id': 5,
+            'away_id': 6,
+            'home_name': 'Ohio State',
+            'away_name': 'Baylor',
+            'home_projection': 69,
+            'away_projection': 55
+        })
 
 
 @app.route("/teams/")
@@ -113,9 +224,6 @@ def teams():
     try:
         teams = Team.query.all()
         return jsonify([t.serialize() for t in teams])
-        # Todo: Return actual data from database
-        # Right now it is empty, so we're returning dummy data
-        # return jsonify([{"id": 1, "name": "Gonazaga"}, {"id": 2, "name": "Duke"}, {"id": 3, "name": "Kansas"}, {"id": 4, "name": "Oregon"}, {"id": 5, "name": "Ohio State"}, {"id": 6, "name": "Baylor"}])
     except Exception as e:
         return(str(e))
 
--- models.py
+++ models.py
@@ -6,10 +6,11 @@ from app import db
 class Team(db.Model):
     __tablename__ = 'team'
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.String(), primary_key=True)
     name = db.Column(db.String(), nullable=False, unique=True)
 
-    def __init__(self, name):
+    def __init__(self, id, name):
+        self.id = id
         self.name = name
 
     def __repr__(self):
@@ -25,23 +26,21 @@ class Team(db.Model):
 class Game(db.Model):
     __tablename__ = 'game'
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     date = db.Column(db.Date(), nullable=False, primary_key=True)
-    home = db.Column(db.Integer(), db.ForeignKey(
+    home = db.Column(db.String(), db.ForeignKey(
         "team.id"), nullable=False, primary_key=True)
-    away = db.Column(db.Integer(), db.ForeignKey(
+    away = db.Column(db.String(), db.ForeignKey(
         "team.id"), nullable=False, primary_key=True)
-    home_score = db.Column(db.Integer())
-    away_score = db.Column(db.Integer())
+    home_score = db.Column(db.Integer(), nullable=True)
+    away_score = db.Column(db.Integer(), nullable=True)
     home_projection = db.Column(db.Float())
     away_projection = db.Column(db.Float())
 
-    def __init__(self, date, home, away, home_score, away_score, home_projection, away_projection):
+    def __init__(self, date, home, away, home_projection, away_projection):
         self.date = date
         self.home = home
         self.away = away
-        self.home_score = home_score
-        self.away_score = away_score
         self.home_projection = home_projection
         self.away_projection = away_projection
 
@@ -52,8 +51,8 @@ class Game(db.Model):
         return {
             'id': self.id,
             'date': self.date,
-            'home': self.home,
-            'away': self.away,
+            'home_id': self.home,
+            'away_id': self.away,
             'home_score': self.home_score,
             'away_score': self.away_score,
             'home_projection': self.home_projection,
